26644d33e9842be2789d52be1e0c23244c50bd6f
Daniël Hansen <info@danielhansen.nl>
2018-09-01 11:12:17 +0200

    Add charity endoints

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Authors:
  - Rick van der Linden
  - Rody Moolenaar
  - Daniël Hansen
+ 
+ ## Setup
+ 
+```bash
+composer install
+php ./bin/do-good orm:schema-tool:update --force
+```
---
+++ src/Controller/CharityController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Bunq\DoGood\Controller;
+
+use Slim\Http\Response;
+use Slim\Http\Request;
+
+/**
+ * Class CharityController
+ * @package Bunq\DoGood\Controller
+ */
+class CharityController extends BaseController
+{
+    /**
+     * Full list of available
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     *
+     * @return Response
+     */
+    public function getList(Request $request, Response $response, array $args) {
+        $path = realpath(__DIR__ . "/../../var/charities.json");
+
+        if ($path === false) {
+            return $this->errorJsonResponse($response, 'Charity data missing');
+        }
+
+        $charities = json_decode(file_get_contents($path));
+
+        return $this->successJsonResponsePayload($response, $charities->categories);
+    }
+}
