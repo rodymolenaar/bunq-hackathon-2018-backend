c69ed31a7bbd31e1a76c7846308719bce3afd6ae
Rick van der Linden <rickvdl@me.com>
2018-09-01 11:00:03 +0200

    Added /merchants endpoint, context still has to be loaded

---
+++ src/Controller/MerchantController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Bunq\DoGood\Controller;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+/**
+ * Class BunqController
+ * @package Bunq\DoGood\controller
+ */
+class MerchantController extends BaseController
+{
+
+  /**
+   * @param Request $request
+   * @param Response $response
+   * @param array $args
+   * @return Response
+   */
+  public function getMerchants(Request $request, Response $response, array $args) {
+      $bunq = $this->get('bunqLib');
+
+      try {
+
+        // Fetch the merchants from bunq
+        $merchants = $bunq->getCardPaymentLocations();
+
+        return $this->successJsonResponsePayload($response, $merchants);
+      } catch(BadRequestException $e) {
+        return $this->errorJsonResponse($response, "An error occured while fetching the merchants");
+      }
+  }
+}
